1ced241e08d8d3a4d374cad2e6814cdd56d33d74
ankitanandBCA <ankitanandraj9563@gmail.com>
2024-11-02 06:05:11 +0000

    basic

---
+++ Basic/variavle.js
@@ -0,0 +1,16 @@
+const accid=1111
+let accEmail="harsh@gmail.com"
+var accPword="121212"
+accCity="pune"
+
+
+//const can't be modify
+//accid=2222
+//console.log(accid);
+
+accEmail="a@gmail.com"
+accPword="77777"
+accCity="bihar"
+
+console.table([accid,accEmail,accPword,accPword])
+
